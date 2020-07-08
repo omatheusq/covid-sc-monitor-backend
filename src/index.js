247b7f70c6353da0f0e44221560c5f9775b2f73c
matheuzquadros <matheuzquadros@gmail.com>
2020-07-07 22:09:33 -0300

    route test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors')
-//const routes = require('./routes')
+const routes = require('./routes')
 
-require('./case-downloader')
+//require('./case-downloader')
+
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+
+app.use(routes)
 
 app.listen(3333)
