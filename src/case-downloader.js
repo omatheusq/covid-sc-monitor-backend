3487a04ee92dc357132f01091738839abdb1874b
matheuzquadros <matheuzquadros@gmail.com>
2020-07-07 21:57:32 -0300

    initial commit

---
+++ src/case-downloader.js
@@ -0,0 +1,44 @@
+var https = require('https');
+var fs = require('fs');
+const CSVToJSON = require('csvtojson');
+const gunzip = require('gunzip-file')
+
+const fileToDownload = 'https://data.brasil.io/dataset/covid19/caso_full.csv.gz';
+
+let saveDataInJSON = (data)=> {
+  fs.writeFile ("./data/casos_sc.json", JSON.stringify(data), ()=> {
+    console.info('JSON file created')
+  })
+}
+
+let readCSV = () => {
+  CSVToJSON().fromFile('./data/caso_full.csv')
+    .then(data => {
+      data = data.filter(d=> d.state === 'SC')
+      saveDataInJSON(data)
+    }).catch(err => {
+      console.log(err);
+    });
+}
+
+let unzipFile = () => {
+
+  console.info('zip file downloaded')
+
+  return gunzip('./data/data.gz', './data/caso_full.csv', () => {
+    console.info('file unzipped')
+    readCSV()
+  })
+}
+
+https.get(fileToDownload, (res) => {
+  if (res.statusCode < 300) {
+
+    let prom = fs.createWriteStream('./data/data.gz')
+
+    prom.on('close', unzipFile)
+
+    res.pipe(prom)
+
+  }
+})
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors')
+//const routes = require('./routes')
+
+require('./case-downloader')
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+app.listen(3333)
