e3650c9d4c316f1d0be9a45fc02ecc1e70f76662
matheuzquadros <matheuzquadros@gmail.com>
2020-07-13 22:23:27 -0300

    Adicionada rotina automática de atualização

--- src/case-downloader.js
+++ src/case-downloader.js
@@ -2,11 +2,12 @@ var https = require('https');
 var fs = require('fs');
 const CSVToJSON = require('csvtojson');
 const gunzip = require('gunzip-file')
+const cron = require('node-cron');
 
 const fileToDownload = 'https://data.brasil.io/dataset/covid19/caso_full.csv.gz';
 
-let saveDataInJSON = (data)=> {
-  fs.writeFile ("./data/casos_sc.json", JSON.stringify(data), ()=> {
+let saveDataInJSON = (data) => {
+  fs.writeFile("./data/casos_sc.json", JSON.stringify(data), () => {
     console.info('JSON file created')
   })
 }
@@ -14,7 +15,7 @@ let saveDataInJSON = (data)=> {
 let readCSV = () => {
   CSVToJSON().fromFile('./data/caso_full.csv')
     .then(data => {
-      data = data.filter(d=> d.state === 'SC')
+      data = data.filter(d => d.state === 'SC')
       saveDataInJSON(data)
     }).catch(err => {
       console.log(err);
@@ -31,14 +32,20 @@ let unzipFile = () => {
   })
 }
 
-https.get(fileToDownload, (res) => {
-  if (res.statusCode < 300) {
 
-    let prom = fs.createWriteStream('./data/data.gz')
+let downloadData = () => {
+  https.get(fileToDownload, (res) => {
+    if (res.statusCode < 300) {
+      let prom = fs.createWriteStream('./data/data.gz')
+      prom.on('close', unzipFile)
+      res.pipe(prom)
+    }
+  })
+}
 
-    prom.on('close', unzipFile)
+downloadData()
 
-    res.pipe(prom)
-
-  }
-})
+cron.schedule('0 22 * * *', () => {
+  downloadData()
+  console.log(`${JSON.stringify(new Date())} - Rotina de busca automatica de dados executada`)
+});
--- src/routes.js
+++ src/routes.js
@@ -1,38 +1,44 @@
 const express = require('express')
 const routes = express.Router();
 
-let cases = require('../data/casos_sc.json')
+let getCases = () => {
+  let cases = require('../data/casos_sc.json')
 
-cases = cases.map(c => (
-  {
-    city: c.city,
-    cityIbgeCode: c.city_ibge_code,
-    date: c.date,
-    epidemiologicalWeek: c.epidemiological_week,
-    newConfirmed: c.new_confirmed,
-    newDeaths: c.new_deaths,
-    caseCount: c.last_available_confirmed,
-    deathCount: c.last_available_deaths,
-    isLastReport: c.is_last == 'True'
-  }
-))
+  cases = cases.map(c => (
+    {
+      city: c.city,
+      cityIbgeCode: c.city_ibge_code,
+      date: c.date,
+      epidemiologicalWeek: c.epidemiological_week,
+      newConfirmed: c.new_confirmed,
+      newDeaths: c.new_deaths,
+      caseCount: c.last_available_confirmed,
+      deathCount: c.last_available_deaths,
+      isLastReport: c.is_last == 'True'
+    }
+  ))
 
+  return cases;
+}
 
-let last_reports = cases.filter(c => c.isLastReport )
-
-console.log(cases.length)
-console.log(last_reports.length)
-
+let getLatestReports = () => {
+  let cases = getCases()
+  return cases.filter(c => c.isLastReport)
+}
 
 routes.get('/', (req, res) => res.send('Hello World!'))
 
 
 routes.get('/city', (req, res) => {
+  let last_reports = getLatestReports()
   res.json(last_reports);
 })
 
 routes.get('/city/:id', (req, res) => {
+  let last_reports = getLatestReports()
+
   let cityIbgeCode = req.params.id;
+
   let city = last_reports.find(c => c.cityIbgeCode == cityIbgeCode)
 
   res.json(city);
