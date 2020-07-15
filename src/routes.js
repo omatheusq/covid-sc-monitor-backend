744489ff0f3758364a8392dfe78bfcc9d69991f0
matheuzquadros <matheuzquadros@gmail.com>
2020-07-15 20:43:12 -0300

    Adicionado serviço para buscar o histórico de casos

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const moment = require('moment')
+
 const routes = express.Router();
+
 
 let getCases = () => {
   let cases = require('../data/casos_sc.json')
@@ -42,6 +45,44 @@ routes.get('/city/:id', (req, res) => {
   let city = last_reports.find(c => c.cityIbgeCode == cityIbgeCode)
 
   res.json(city);
+})
+
+routes.get('/city/:id/history', (req, res) => {
+  let cityIbgeCode = req.params.id;
+
+  let cases = getCases()
+
+
+  const groups = cases.filter(c=> c.cityIbgeCode == cityIbgeCode).reduce((acc, c) => {
+
+    // create a composed key: 'year-week' 
+    const yearWeek = `${moment(c.date).year()}-${moment(c.date).week()}`;
+    
+    // add this key as a property to the result object
+    if (!acc[yearWeek]) {
+      acc[yearWeek] = [];
+    }
+    
+    // push the current date that belongs to the year-week calculated befor
+    acc[yearWeek].push(c);
+  
+    return acc;
+  
+  }, {});
+
+  let data = []
+  for(key in groups){
+    let items = groups[key]
+    data.push(items[items.length - 1])
+  }
+
+  cases = data
+    .map(c=> ({
+      cases: parseInt(c.caseCount),
+      deaths: parseInt(c.deathCount),
+      date: c.date
+    }))
+  res.json(cases);
 })
 
 module.exports = routes;
